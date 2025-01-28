ae7a447b52b858f09dcd453c34cfe5566eac0221
magicsight2 <93862373+magicsight2@users.noreply.github.com>
2025-01-28 14:52:12 +0530

    Update fixed.c

--- fixed.c
+++ fixed.c
@@ -1,47 +1,45 @@
 include<stdio.h>
 
-2 #include<math.h>
+#include<math.h>
 
-3 float gx(float x)
+float gx(float x)
 
-4{
+{
 
-5 float j;
+float j;
 
-6j=0.5*(sqrt(10 pow(x,3)));
+j=0.5*(sqrt(10 pow(x,3)));
 
-7 return j;
+return j;
 
-8}
+}
 
-9 float fx(float x)
+float fx(float x)
 
-10 {
+{
 
-11 float i;
+float i;
 
-12 i = pow(x,3) + 4*pow(x,2) - 10;
+i = pow(x,3) + 4*pow(x,2) - 10;
 
-13 return i;
+return i;
 
-14}
+}
 
-15 float main()
+float main()
 
-16
+float x2, x1=1,e=0.001;
 
-17 float x2, x1=1,e=0.001;
+do
 
-18 do
+{
 
-19 {
+x2 = gx(x1);
 
-20 x2 = gx(x1);
+x1 = x2;
 
-21 x1 = x2;
+} while(fabs(fx(x2))>e);
 
-22} while(fabs(fx(x2))>e);
+printf("the solution of the equation is %f \n",x2);
 
-23 printf("the solution of the equation is %f \n",x2);
 
-24
